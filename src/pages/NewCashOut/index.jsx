4f76bd7a618082c5f7bfe8ce306288210e61627a
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-01-22 21:06:22 -0300

    Implemented cash out function

--- src/pages/NewCashOut/index.jsx
+++ src/pages/NewCashOut/index.jsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import axios from "axios";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../App";
 import BtnFullWidth from "../../components/BtnFullWidth";
 import Input from "../../components/Input";
 import PageTitle from "../../components/PageTitle";
@@ -7,12 +10,69 @@ import NewCashOutStyle from "./style";
 export default function NewCashOut() {
   const [value, setValue] = useState();
   const [description, setDescription] = useState("");
+  const [session, setSession] = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  async function addCashOut(e) {
+    e.preventDefault();
+
+    if (!session) {
+      const localSession = localStorage.getItem("session");
+      if (localSession) {
+        setSession(JSON.parse(localSession));
+        return;
+      } else {
+        navigate("/");
+        return;
+      }
+    }
+
+    const newCashOut = {
+      description: description,
+      value: Number(value),
+      type: "out",
+    };
+
+    try {
+      const cashInPromise = axios.post(
+        "http://localhost:5000/registers",
+        newCashOut,
+        {
+          headers: {
+            authorization: `Bearer ${session.token}`,
+            user_id: session.userId,
+          },
+        }
+      );
+
+      cashInPromise.then(() => {
+        alert("Saida feita com sucesso.");
+        navigate("/home");
+      });
+
+      cashInPromise.catch((error) => {
+        if (error.response.status === 401) {
+          localStorage.removeItem("session");
+          setSession(undefined);
+          alert("Sua sessão expirou!");
+          navigate("/");
+        }
+        if (error.response.status === 422) {
+          error.response.data?.forEach((err) => {
+            alert(err.message);
+          });
+        }
+      });
+    } catch (error) {
+      alert("Algo deu errado ao enviar a saida");
+    }
+  }
 
   return (
     <NewCashOutStyle>
       <PageTitle text="Nova saída" />
 
-      <form>
+      <form onSubmit={addCashOut}>
         <Input
           type="text"
           value={value}
